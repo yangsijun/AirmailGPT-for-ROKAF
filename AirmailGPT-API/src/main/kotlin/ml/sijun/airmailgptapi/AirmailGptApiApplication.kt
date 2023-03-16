2071cfe91bd3874449cbf00cb3b5f99e5989bde1
양시준 <sijun0905@gmail.com>
2023-03-16 17:50:25 +0900

    Create Backend API Project

---
+++ AirmailGPT-API/src/main/kotlin/ml/sijun/airmailgptapi/AirmailGptApiApplication.kt
@@ -0,0 +1,11 @@
+package ml.sijun.airmailgptapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AirmailGptApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AirmailGptApiApplication>(*args)
+}
---
+++ AirmailGPT-API/src/test/kotlin/ml/sijun/airmailgptapi/AirmailGptApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package ml.sijun.airmailgptapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AirmailGptApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
