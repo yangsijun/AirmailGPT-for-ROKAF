87f4d756bcb3b53c809c87262f082714988ef8d7
양시준 <sijun0905@gmail.com>
2023-03-21 06:37:49 +0900

    Add Setting.kt (template)

---
+++ AirmailGPT-API/src/main/kotlin/ml/sijun/airmailgptapi/settingTemplate.kt
@@ -0,0 +1,7 @@
+package ml.sijun.airmailgptapi
+
+//const val SENDER_ZIPCODE = "37554"
+//const val SENDER_ADDR1 = "경상북도 포항시 북구 흥해읍 한동로 558"
+//const val SENDER_ADDR2 = "AirmailGPT-for-ROKAF"
+//const val ENLIST_DATE = "2023년 4월 24일"
+//const val OPENAI_API_KEY = "OPENAI_API_KEY"
